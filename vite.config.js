809f8475879ced6ce22b1fcba9ca431f36f1e918
osmin281198 <osminbackup@gmail.com>
2026-06-18 11:39:59 +0700

    perf: code splitting vite config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,29 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-    server: {
-        watch: {
-              usePolling: true,
-                    interval: 1000,
-                        },
-                          },
-                          })
+  build: {
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': [
+            'react',
+            'react-dom',
+            'react-router-dom',
+          ],
+          'supabase-vendor': [
+            '@supabase/supabase-js',
+          ],
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      '@supabase/supabase-js',
+    ]
+  }
+})
